2c8e0d39790823d33b03fede593a5c60fb9ee63f
HAM!DREZA <hamidreza.eidy1999@gmail.com>
2023-12-14 12:36:23 +0330

    create interface and add some classes to ts file

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,43 @@
 "use strict";
-console.log("hi");
+class Todo {
+    constructor(todo) {
+        this.id = todo.id;
+        this.title = todo.title;
+        this.status = todo.status;
+    }
+}
+class UI {
+    addTodoToList(todo) {
+        const list = document.getElementById('todo-list');
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <th>${todo.id}</th>
+        <td>${todo.title}</td>
+        <td><input type="checkbox" ${todo.status ? "checked" : ""} class="form-check-input"></td>
+        <td><button class="btn btn-sm btn-outline-danger" onclick="ui.removeTodo(event)">delete</button></td>
+        `;
+        list.appendChild(tr);
+    }
+}
+const form = document.getElementById('todo-form');
+const title = document.getElementById('title');
+const titleError = document.getElementById('title-error');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (title.value === '') {
+        titleError.innerHTML = 'Title is required...';
+    }
+    else {
+        titleError.innerHTML = '';
+        console.log(title.value);
+        const todoObj = {
+            id: Math.round(Math.random() * 100),
+            title: title.value.trim(),
+            status: false
+        };
+        const todo = new Todo(todoObj);
+        const ui = new UI();
+        ui.addTodoToList(todo);
+        title.value = '';
+    }
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,56 @@
-console.log("hi");
+interface TodoInterface {
+    id: number,
+    title: string,
+    status: boolean
+}
+
+class Todo implements TodoInterface {
+    id: number;
+    title: string;
+    status: boolean;
+
+    constructor(todo: TodoInterface) {
+        this.id = todo.id;
+        this.title = todo.title;
+        this.status = todo.status;
+    }
+}
+
+class UI {
+    addTodoToList(todo: TodoInterface) {
+        const list = document.getElementById('todo-list')!;
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <th>${todo.id}</th>
+        <td>${todo.title}</td>
+        <td><input type="checkbox" ${todo.status ? "checked" : ""} class="form-check-input"></td>
+        <td><button class="btn btn-sm btn-outline-danger" onclick="ui.removeTodo(event)">delete</button></td>
+        `;
+        list.appendChild(tr);
+    }
+}
+const form = document.getElementById('todo-form') as HTMLFormElement;
+const title = document.getElementById('title') as HTMLInputElement;
+const titleError = document.getElementById('title-error') as HTMLParagraphElement;
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault()
+    if (title.value === '') {
+        titleError.innerHTML = 'Title is required...'
+
+    } else {
+        titleError.innerHTML = ''
+        console.log(title.value);
+        const todoObj: TodoInterface = {
+            id: Math.round(Math.random() * 100),
+            title: title.value.trim(),
+            status: false
+        }
+        const todo = new Todo(todoObj)
+    const ui = new UI()
+    ui.addTodoToList(todo)
+    title.value = ''
+
+    }
+
+})
